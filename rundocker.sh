ab256e1d1f3514295faa410445c5ed0800a7107c
Rich Magahiz <rich@toutapp.com>
2014-11-20 15:35:49 -0800

    Map the /elasticsearch/data directory only

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export ES_PERSIST="-v /mnt/elasticsearch:/elasticsearch"
+export ES_PERSIST="-v /mnt/elasticsearch/data:/elasticsearch/data"
 docker run -d -h elk --name elk -p 80:80 -p 3333:3333 -p 3334:3334 -p 9200:9200 ${ES_PERSIST} 4thace/elk /elk_start.sh
